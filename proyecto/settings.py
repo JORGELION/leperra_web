866e5b669bd77b6bc7bb49bf11a913bc4caeb0f2
Jorge Andres Gomez Castillo <jorgeandresgomez007@gmail.com>
2022-10-02 06:11:36 -0500

    comento unas lines en settings.py

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -140,11 +140,11 @@ if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
